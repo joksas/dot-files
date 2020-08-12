70f1983f3b85776f06aeff33980eeb69250b0959
Dovydas <dovydas.joksas.15@ucl.ac.uk>
2020-08-12 07:34:19 +0300

    change layout symbols

--- .config/dwm/config.h
+++ .config/dwm/config.h
@@ -53,8 +53,8 @@ static const int resizehints = 0;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "[T]",      tile },    /* first entry is default */
+	{ "[F]",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 };
 
