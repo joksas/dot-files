cd4e2ff9af37824303c627e14583f5839e908096
Dovydas <dovydas.joksas.15@ucl.ac.uk>
2020-08-13 10:30:14 +0300

    change update times

--- .config/dwmblocks/blocks.def.h
+++ .config/dwmblocks/blocks.def.h
@@ -2,10 +2,10 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{" ", "volume-info",					0,		10},
-	{"", "battery-info",					3600,		0},
-	{"", "temperature-info",				60,		0},
-	{"", "disk-info",					600,		0},
-	{"", "date-info",					3600,		0},
+	{"", "battery-info",					120,		0},
+	{"", "temperature-info",				10,		0},
+	{"", "disk-info",					120,		0},
+	{"", "date-info",					180,		0},
 	{"", "time-info",					1,		0},
 };
 
