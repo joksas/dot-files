34d69119d39221d2162b8e2412a3d733aba45981
Dovydas <dovydas@yoshke.org>
2021-02-16 10:35:38 +0000

    change font

--- .config/st/config.def.h
+++ .config/st/config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Source Code Pro:pixelsize=20:antialias=true:autohint=true";
+static char *font = "Iosevka:pixelsize=22:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
