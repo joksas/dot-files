bbf11cccffb65a520bd1aa65a6a4e7cb2400cccd
Dovydas <dovydas@yoshke.org>
2021-06-06 18:21:06 +0100

    update settings for 4k

--- .config/st/config.def.h
+++ .config/st/config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Iosevka:pixelsize=22:antialias=true:autohint=true";
+static char *font = "Iosevka:pixelsize=30:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
