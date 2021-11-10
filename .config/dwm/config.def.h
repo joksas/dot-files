0b6ebae3045a14884e675d9b6194c80df4644da6
Dovydas Joksas <dovydas@yoshke.org>
2021-11-10 19:53:34 +0000

    patch `sticky`

--- .config/dwm/config.def.h
+++ .config/dwm/config.def.h
@@ -7,8 +7,8 @@ static const unsigned int gappx    = 7;        /* gap pixel between windows */
 static const unsigned int snap     = 32;       /* snap pixel */
 static const int showbar           = 1;        /* 0 means no bar */
 static const int topbar            = 1;        /* 0 means bottom bar */
-static const char *fonts[]         = { "monospace:size=14", "Font Awesome 5 Free:size=14", "Font Awesome 5 Brands:size=14"};
-static const char dmenufont[]      = "monospace:size=14";
+static const char *fonts[]         = {"Iosevka:size=14", "FontAwesome5Brands:size=14:antialias:true", "FontAwesome5Free:size=14:antialias:true", "FontAwesome5Free:style=Solid:size=14:antialias:true"};
+static const char dmenufont[]      = "Iosevka:size=14";
 static const char col_gray1[]      = "#222222";
 static const char col_gray2[]      = "#444444";
 static const char col_gray3[]      = "#bbbbbb";
@@ -19,28 +19,28 @@ static const char col_white[]      = "#ffffff";
 static const char col_gray[]       = "#4d4d4d";
 static const char col_dark_gray[]  = "#101010";
 static const char *colors[][3]     = {
-  /*               fg         bg         border   */
-  [SchemeNorm]               = { col_white, col_dark_gray, col_gray },
-  [SchemeSel]                = { col_dark_gray, col_white,  col_blue  },
+    /*               fg         bg         border   */
+    [SchemeNorm]               = { col_white, col_dark_gray, col_gray },
+    [SchemeSel]                = { col_dark_gray, col_white,  col_blue  },
 };
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
-  /* xprop(1):
-   *	WM_CLASS(STRING) = instance, class
-   *	WM_NAME(STRING) = title
-   */
-  /* class      instance    title       tags mask     isfloating   monitor */
-  { "Gimp",     NULL,       NULL,       0,            0,           -1 },
-  { "Brave-browser",  NULL,       NULL,       1 << 0,       0,           -1 },
-  { "st-256color",  NULL,       NULL,       1 << 1,       0,           -1 }, 
-  { "okular",  NULL,       NULL,       1 << 2,       0,           -1 }, 
-  { "zoom",  NULL,       NULL,       1 << 8,       0,           -1 }, 
-  { "Skype",  NULL,       NULL,       1 << 8,       0,           -1 }, 
-  { "Microsoft Teams - Preview",  NULL,       NULL,       1 << 8,       0,           -1 }, 
-  { "WxWabbitemu",     NULL,       NULL,       0,            1,           -1 },
+    /* xprop(1):
+     *	WM_CLASS(STRING) = instance, class
+     *	WM_NAME(STRING) = title
+     */
+    /* class      instance    title       tags mask     isfloating   monitor */
+    { "Gimp",     NULL,       NULL,       0,            0,           -1 },
+    { "Brave-browser",  NULL,       NULL,       1 << 0,       0,           -1 },
+    { "st-256color",  NULL,       NULL,       1 << 1,       0,           -1 },
+    { "okular",  NULL,       NULL,       1 << 2,       0,           -1 },
+    { "zoom",  NULL,       NULL,       1 << 8,       0,           -1 },
+    { "Skype",  NULL,       NULL,       1 << 8,       0,           -1 },
+    { "Microsoft Teams - Preview",  NULL,       NULL,       1 << 8,       0,           -1 },
+    { "WxWabbitemu",     NULL,       NULL,       0,            1,           -1 },
 };
 
 /* layout(s) */
@@ -49,12 +49,12 @@ static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
-  /* symbol     arrange function */
-  { "[T]",      tile },    /* first entry is default */
-  { "[F]",      NULL },    /* no layout function means floating behavior */
-  { "[M]",      monocle },
-  { "|C|",      centeredmaster },
-  { "<C>",      centeredfloatingmaster },
+    /* symbol     arrange function */
+    { "[T]",      tile },    /* first entry is default */
+    { "[F]",      NULL },    /* no layout function means floating behavior */
+    { "[M]",      monocle },
+    { "|C|",      centeredmaster },
+    { "<C>",      centeredfloatingmaster },
 };
 
 /* key definitions */
@@ -74,39 +74,40 @@ static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont,
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
-  /* modifier                     key        function        argument */
-  { MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-  { MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-  { MODKEY|ShiftMask,             XK_r,      spawn,          SHCMD("st ranger") },
-  { MODKEY|ShiftMask,             XK_l,      spawn,          SHCMD("i3lock-fancy-dualmonitor") },
-  { MODKEY|ShiftMask,             XK_b,      spawn,          SHCMD("brave") },
-  { MODKEY,                       XK_b,      togglebar,      {0} },
-  { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-  { MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-  { MODKEY|ControlMask,                       XK_i,      incnmaster,     {.i = +1 } },
-  { MODKEY|ControlMask,                       XK_d,      incnmaster,     {.i = -1 } },
-  { MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-  { MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-  { MODKEY,                       XK_Return, zoom,           {0} },
-  { MODKEY,                       XK_Tab,    view,           {0} },
-  { MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-  { MODKEY|ControlMask,           XK_t,      setlayout,      {.v = &layouts[0]} },
-  { MODKEY|ControlMask,           XK_f,      setlayout,      {.v = &layouts[1]} },
-  { MODKEY|ControlMask,           XK_m,      setlayout,      {.v = &layouts[2]} },
-  { MODKEY|ControlMask,           XK_u,      setlayout,      {.v = &layouts[3]} },
-  { MODKEY|ControlMask,           XK_o,      setlayout,      {.v = &layouts[4]} },
-  { MODKEY,                       XK_space,  setlayout,      {0} },
-  { MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-  { MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-  { MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-  { MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-  { MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-  { MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-  { MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-  { MODKEY,                       XK_u,      spawn,          SHCMD("udiskie-mount /dev/sdb1 && cd /run/media/dovydas && st ranger") },
-  { MODKEY|ShiftMask,             XK_u,      spawn,          SHCMD("udiskie-umount /dev/sdb1 && notify-send -t 3000 \"/dev/sdb1 unmounted\"") },
-  { MODKEY,                       XK_Print,  spawn,          SHCMD("import png:- | xclip -selection clipboard -t image/png") },
-  TAGKEYS(                        XK_1,                      0)
+    /* modifier                     key        function        argument */
+    { MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+    { MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+    { MODKEY|ShiftMask,             XK_r,      spawn,          SHCMD("st ranger") },
+    { MODKEY|ShiftMask,             XK_l,      spawn,          SHCMD("i3lock-fancy-dualmonitor") },
+    { MODKEY|ShiftMask,             XK_b,      spawn,          SHCMD("brave") },
+    { MODKEY,                       XK_b,      togglebar,      {0} },
+    { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
+    { MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+    { MODKEY|ControlMask,                       XK_i,      incnmaster,     {.i = +1 } },
+    { MODKEY|ControlMask,                       XK_d,      incnmaster,     {.i = -1 } },
+    { MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
+    { MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+    { MODKEY,                       XK_Return, zoom,           {0} },
+    { MODKEY,                       XK_Tab,    view,           {0} },
+    { MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+    { MODKEY|ControlMask,           XK_t,      setlayout,      {.v = &layouts[0]} },
+    { MODKEY|ControlMask,           XK_f,      setlayout,      {.v = &layouts[1]} },
+    { MODKEY|ControlMask,           XK_m,      setlayout,      {.v = &layouts[2]} },
+    { MODKEY|ControlMask,           XK_u,      setlayout,      {.v = &layouts[3]} },
+    { MODKEY|ControlMask,           XK_o,      setlayout,      {.v = &layouts[4]} },
+    { MODKEY,                       XK_space,  setlayout,      {0} },
+    { MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+    { MODKEY,                       XK_s,      togglesticky,   {0} },
+    { MODKEY,                       XK_0,      view,           {.ui = ~0 } },
+    { MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+    { MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
+    { MODKEY,                       XK_period, focusmon,       {.i = +1 } },
+    { MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
+    { MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+    { MODKEY,                       XK_u,      spawn,          SHCMD("udiskie-mount /dev/sdb1 && cd /run/media/dovydas && st ranger") },
+    { MODKEY|ShiftMask,             XK_u,      spawn,          SHCMD("udiskie-umount /dev/sdb1 && notify-send -t 3000 \"/dev/sdb1 unmounted\"") },
+    { MODKEY,                       XK_Print,  spawn,          SHCMD("import png:- | xclip -selection clipboard -t image/png") },
+    TAGKEYS(                        XK_1,                      0)
     TAGKEYS(                        XK_2,                      1)
     TAGKEYS(                        XK_3,                      2)
     TAGKEYS(                        XK_4,                      3)
@@ -115,7 +116,9 @@ static Key keys[] = {
     TAGKEYS(                        XK_7,                      6)
     TAGKEYS(                        XK_8,                      7)
     TAGKEYS(                        XK_9,                      8)
-    { MODKEY|ControlMask|ShiftMask,             XK_q,      quit,           {0} },
+    {
+        MODKEY|ControlMask|ShiftMask,             XK_q,      quit,           {0}
+    },
     { 0,                            XF86XK_AudioLowerVolume, spawn, SHCMD("amixer set Master 5%- && kill -44 $(pidof dwmblocks)") },
     { 0,                            XF86XK_AudioRaiseVolume, spawn, SHCMD("amixer set Master 5%+ && kill -44 $(pidof dwmblocks)") },
     { 0,                            XF86XK_AudioMute, spawn, SHCMD("amixer set Master toggle && kill -44 $(pidof dwmblocks)") },
@@ -124,17 +127,17 @@ static Key keys[] = {
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
-  /* click                event mask      button          function        argument */
-  { ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-  { ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
-  { ClkWinTitle,          0,              Button2,        zoom,           {0} },
-  { ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
-  { ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
-  { ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
-  { ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
-  { ClkTagBar,            0,              Button1,        view,           {0} },
-  { ClkTagBar,            0,              Button3,        toggleview,     {0} },
-  { ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
-  { ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+    /* click                event mask      button          function        argument */
+    { ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
+    { ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+    { ClkWinTitle,          0,              Button2,        zoom,           {0} },
+    { ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+    { ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
+    { ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
+    { ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+    { ClkTagBar,            0,              Button1,        view,           {0} },
+    { ClkTagBar,            0,              Button3,        toggleview,     {0} },
+    { ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+    { ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
 
